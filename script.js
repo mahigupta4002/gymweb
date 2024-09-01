8a43ebb69b7284a7334b9d95cb77697f7290fc63
mahigupta4002 <mahigupta4002@gmail.com>
2024-09-01 17:38:03 +0530

    final

---
+++ script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.oneclick = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// typying text code 
+const typed = new Typed('.multiple-text', {
+    strings: ['Physical Fitness', 'Sports-Oriented Training','Weight-Gain','Fat-Lose'],
+    typeSpeed: 60,
+    backSpeed:60,
+    backDelay:1000,
+    loop:true,
+  });
+
+
